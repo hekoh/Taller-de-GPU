ae8045a9a450e0d426bc3eeb78f719ba07ebf46c
hekoh <lucagerace@hotmail.com>
2026-09-18 10:32:57 -0300

    corrijo ejercicio 4

--- Practica/Practica-1/soluciones/E4.cu
+++ Practica/Practica-1/soluciones/E4.cu
@@ -3,12 +3,13 @@
 #include<float.h>
 #include<sys/time.h>
 #include<sys/resource.h>
+#include <math.h>
 
 
-#define CUDA_BLK 64
+#define CUDA_BLK 32
 
 
-int *h_A, *h_B, *h_C;
+int *h_A, *h_B, *h_C, *h_check;
 int *d_A, *d_B, *d_C;
 
 __constant__ int d_N;
@@ -17,6 +18,15 @@ int N;
 double dwalltime();
 __global__ void funcionGPU(int N, int *A, int *B, int *C);
 
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+static void HandleError( cudaError_t err, const char *file, int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
 
 
 
@@ -38,12 +48,14 @@ int main(int argc, char *argv[])
    h_A = (int *) malloc(N*N*sizeof(int));
    h_B = (int *) malloc(N*N*sizeof(int));
    h_C = (int *) malloc(N*N*sizeof(int));    
+   h_check = (int *) malloc(N*N*sizeof(int));
 
    for(int i = 0; i < N; i++){
 	for(int j = 0; j < N; j++){
 	    h_A[i*N +j] = rand() % N; //inicializo por filas
 	    h_B[i+N*j] = rand() % N;  //inicializo por columnas
 	    h_C[i*N+j] = 0;  
+	    h_check[i*N+j] = 0;
 	}
    }
    printf("tiempo de alocacion y inicializacion en CPU: %f\n", dwalltime() - timetick);
@@ -58,32 +70,46 @@ int main(int argc, char *argv[])
    printf("tiempo de alocacion en memoria: %f \n",dwalltime() - timetick);
 
    timetick = dwalltime();
-   cudaMemcpy(d_A, h_A, N*N*sizeof(int), cudaMemcpyHostToDevice); // CPU -> GPU
-   cudaMemcpy(d_B, h_B, N*N*sizeof(int), cudaMemcpyHostToDevice);
-   cudaMemcpyToSymbol(d_N,&N,sizeof(int));
+   HANDLE_ERROR(cudaMemcpy(d_A, h_A, N*N*sizeof(int), cudaMemcpyHostToDevice)); // CPU -> GPU
+   HANDLE_ERROR(cudaMemcpy(d_B, h_B, N*N*sizeof(int), cudaMemcpyHostToDevice));
+   HANDLE_ERROR(cudaMemcpyToSymbol(d_N,&N,sizeof(int)));
    printf("tiempo de copia de memoria CPU => GPU: %f \n",dwalltime() - timetick);
 
-   dim3 dimGrid(N*N / CUDA_BLK,1,1);	//defino grid y bloque
-   dim3 dimBlock(CUDA_BLK);
+   dim3 dimGrid(N / CUDA_BLK, N / CUDA_BLK,1);	//defino grid y bloque
+   dim3 dimBlock(CUDA_BLK, CUDA_BLK);
    
    timetick = dwalltime();
    funcionGPU<<<dimGrid,dimBlock>>>(N, d_A, d_B, d_C);  //ejecuto kernel en GPU
+   HANDLE_ERROR( cudaPeekAtLastError() );
+   HANDLE_ERROR( cudaDeviceSynchronize() );
    cudaDeviceSynchronize();	//sincronizo los hilos
    
    printf("\nTiempo de computo en GPU: %f",dwalltime() - timetick);
 
    cudaMemcpy(h_C, d_C, N*N*sizeof(int), cudaMemcpyDeviceToHost); // GPU -> CPU
 
+
+/********************** testeo resultado ********************/
+
    for(int i = 0; i < N; i++){
 	for(int j = 0; j < N; j++){
-	    check = check && (h_C[i*N+j] == h_A[i*N+j] + h_B[i+N*j]);
+	   for(int k = 0; k < N; k++)
+		h_check[i*N+j] += h_A[i*N+k] * h_B[k+j*N];
 	}
    }
+
+   for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+		check = check && (h_check[i*N+j] == h_C[i*N+j]);
+		printf("check: %d   gpu: %d\n",h_check[i*N+j],h_C[i*N+j]);
+	}
+   }
+
    
    if(check)
- 	printf("\nLas matrices fueron sumadas correctamente\n");
+ 	printf("\nLas matrices fueron multiplicadas correctamente\n");
    else
-	printf("\nResultado erroneo, las matrices no fueron sumadas correctamente\n");
+	printf("\nResultado erroneo, las matrices no fueron multiplicadas correctamente\n");
 
 
    free(h_A);
@@ -92,6 +118,7 @@ int main(int argc, char *argv[])
    cudaFree(d_B);
    free(h_C);
    cudaFree(d_C);
+   free(h_check);
 }
 
 
@@ -99,16 +126,15 @@ int main(int argc, char *argv[])
 
 
 __global__ void funcionGPU (int N, int *A, int *B, int *C){
-   
-   int id = blockIdx.x * blockDim.x + threadIdx.x;
-   if(id < N*N){
-	for(int i = 0; i < N; i++){
-	   for(int j = 0; i < N; j++){
-		C[id] = A[i*N+j] + B[j*N+i];
-	   }
+   int idX = blockIdx.x * blockDim.x + threadIdx.x;
+   int idY = blockIdx.y * blockDim.y + threadIdx.y;
+   if(idX < N && idY < N){
+	for(int k = 0; k < N; k++){
+		C[idY*N+idX] += A[idY*N+k] * B[k+idX*N];
 	}
    }
 }
+
 	   
    
    
